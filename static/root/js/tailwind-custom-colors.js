e9a1e0281f35ca0503cc6374d95501162c6b750b
Levi Chinecherem Chidi <74856946+Levi-Chinecherem@users.noreply.github.com>
2024-03-01 02:38:32 +0100

    collected statics

---
+++ static/root/js/tailwind-custom-colors.js
@@ -0,0 +1,24 @@
+// tailwind-custom-colors.js
+
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          // Define your custom colors here
+          text: '#333333',
+          button: '#FF0000', // Red color for buttons
+          buttonText: '#FFFFFF', // White color for button text
+          buttonHover: '#CC0000', // Darker red for button hover
+          navLink: '#555555',
+          otherLink: '#007BFF',
+          navLinkHover: '#888888',
+          borderColor: '#CCCCCC',
+          backgroundColor: '#F7F7F7',
+          // Add more custom colors as needed
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  };
+  
