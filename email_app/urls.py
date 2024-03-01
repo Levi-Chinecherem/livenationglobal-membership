603417c3b5dc913e3d0d182364777aa3c733c461
Levi Chinecherem Chidi <74856946+Levi-Chinecherem@users.noreply.github.com>
2024-03-01 02:40:08 +0100

    added url config

--- email_app/urls.py
+++ email_app/urls.py
@@ -1,8 +1,14 @@
 # email_app/urls.py
 from django.urls import path
 from .views import send_email, success_view
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
+
+urlpatterns = ([
     path('', send_email, name='send_email'),
     path('success/', success_view, name='success'),
 ]
+    + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
